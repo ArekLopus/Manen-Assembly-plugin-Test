d86b14ca802c4fb4f390a242efc372570e915538
Ark <Ark@RuleZ.com>
2015-06-10 11:40:50 +0200

    1st commit

---
+++ src/main/java/jar/App.java
@@ -0,0 +1,17 @@
+package jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	/**
+     * Creates the main class
+     *
+     * @param args - command line arguments
+     */
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
